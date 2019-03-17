5b58f21dfd5eef7be2aaa6eb8e50ad06d2dbe51e
Jo <36869260+josephxbrick@users.noreply.github.com>
2019-03-17 10:56:45 -0700

    Add files via upload

---
+++ tidyupdocument.sketchplugin/Contents/artboardfunctions.js
@@ -0,0 +1,35 @@
+// figure out if arboards are laid out horizontally or vertically.
+// assumes no artboads overlap.
+function getOrientation(artboards){
+	if (artboards.count() == 1){
+		return 'horizontal';
+	} else {
+		let widthSum = 0;
+		let minX = Number.MAX_SAFE_INTEGER;
+		let maxX = Number.MIN_SAFE_INTEGER;
+		for (let i = 0; i < artboards.count(); i++){
+			let artboard = artboards[i];
+			widthSum += artboard.frame().width();
+			let left = artboard.frame().x();
+			let right = artboard.frame().x() + artboard.frame().width();
+			minX = Math.min(left, minX);
+			maxX = Math.max(right, maxX);
+		}
+		let delta = Math.max(minX,maxX) - Math.min(minX,maxX);
+		if (delta >= widthSum) {
+			return 'horizontal';
+		} else {
+			return 'vertical'
+		}
+	}
+}
+
+// sort artboads by horizontal or vertical position
+function sortArtboardsByLayout(artboards){
+  let orientation = getOrientation(artboards);
+  if (orientation == 'horizontal'){
+    artboards.sort((a, b) => a.frame().x() - b.frame().x());
+  } else if (orientation == 'vertical'){
+    artboards.sort((a, b) => a.frame().y() - b.frame().y());
+  }
+}
